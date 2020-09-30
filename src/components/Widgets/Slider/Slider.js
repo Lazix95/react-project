baef75d1ef868f22fc91f138d42efc7d657bcdd4
Lazar Matic <lazar.matic@menu.app>
2020-09-30 20:09:02 +0200

    Slider image links changed

--- src/components/Widgets/Slider/Slider.js
+++ src/components/Widgets/Slider/Slider.js
@@ -18,9 +18,9 @@ class SimpleSlider extends React.Component {
       };
       return (
          <Slider {...settings} >
-            <img className={styles.sliderItem} alt={"sliderPic"} src={"http://ibtekar.pt/wp-content/uploads/2016/08/nature-q-c-1920-400-10.jpg"}/>
-            <img className={styles.sliderItem} alt={"sliderPic"}  src={"http://s3.amazonaws.com/inohssivad.com/wp-content/images/2013/05/21134011/open_road-wallpaper-1920x400.jpg"}/>
-            <img className={styles.sliderItem} alt={"sliderPic"}  src={"https://gbico.net/wp-content/uploads/2019/01/19.jpg"}/>
+            <img className={styles.sliderItem} alt={"sliderPic"} src={"https://gbico.net/wp-content/uploads/2019/01/19.jpg"}/>
+            <img className={styles.sliderItem} alt={"sliderPic"} src={"https://gbico.net/wp-content/uploads/2019/01/19.jpg"}/>
+            <img className={styles.sliderItem} alt={"sliderPic"} src={"https://gbico.net/wp-content/uploads/2019/01/19.jpg"}/>
          </Slider>
       );
    }
